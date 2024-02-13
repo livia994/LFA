e0d3c327d4f5823db18aabe94903220cbc446e14
Olivia <olivia.gincu@isa.utm.md>
2024-02-13 16:15:50 +0200

    Define the grammar class with constructor

--- lab1.py
+++ lab1.py
@@ -1,1 +1,25 @@
-print("Hello world!")
+import random
+
+grammar = {
+    'S': ['aS', 'bS', 'cA'],
+    'A': ['aB'],
+    'B': ['aB', 'bB', 'c']
+}
+
+
+def generate_random_string(start_symbol):
+    if start_symbol not in grammar:
+        return start_symbol
+    production = random.choice(grammar[start_symbol])
+    generated_string = ''
+    for symbol in production:
+        generated_string += generate_random_string(symbol)
+    return generated_string
+
+
+if __name__ == "__main__":
+    start_symbol = 'S'
+
+    for _ in range(5):
+        random_string = generate_random_string(start_symbol)
+        print(random_string)
