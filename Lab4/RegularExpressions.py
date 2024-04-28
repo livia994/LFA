7b264923f0026fcedfe35c017a59cf7cef8a6bfe
livia994 <olivia.gincu29@gmail.com>
2024-04-28 19:14:02 +0300

    Regular Expressions

--- Lab4/RegularExpressions.py
+++ Lab4/RegularExpressions.py
@@ -1,97 +1,61 @@
+def generate_combinations(regex):
+    char_sets = {
+        'M': ['N', ''],
+        'N': ['N'],
+        'O': ['O', ''],
+        'P': ['P', ''],
+        'Q': ['Q', ''],
+        'R': ['R'],
+        'x': ['x', 'y', 'z'],
+        'y': ['x', 'y', 'z'],
+        'z': ['x', 'y', 'z'],
+        '8': ['8'],
+        '9': ['9'],
+        '0': ['0'],
+        'H': ['H', 'I'],
+        'I': ['L'],
+        'J': ['J', 'K'],
+        'K': ['L']
+    }
 
-class Grammar:
-    def __init__(self, rules):
-        self.rules = rules
+    combinations = ['']
 
-    def removeLambda(self):
-        nullable_symbols = set()
+    processed_parts = []
 
-        # Step 1: Identify nullable symbols
-        for non_terminal, productions in self.rules.items():
-            if '' in productions:
-                nullable_symbols.add(non_terminal)
+    for part in regex.split():
+        part_combinations = ['']
+        processed_part = ''
 
-        # Step 2: Remove epsilon productions
-        for non_terminal, productions in self.rules.items():
-            updated_productions = [p for p in productions if p != '']
-            self.rules[non_terminal] = updated_productions
+        for char in part:
+            if char in char_sets:
+                part_combinations = [combo + new_char for combo in part_combinations for new_char in char_sets[char]]
+                processed_part += f"({char} → {'|'.join(char_sets[char])}) "
+            elif char.isdigit():
+                part_combinations = [combo * int(char) for combo in part_combinations]
+                processed_part += f"(repeat {char} times) "
+            elif char.startswith("pow"):
+                repeat_part = ['']
+                for sub_char in char.split("(")[1].split(")")[0].split(",")[1:]:
+                    repeat_part = [combo + new_char for combo in repeat_part for new_char in char_sets[sub_char]]
+                part_combinations = [combo1 + combo2 for combo1 in part_combinations for combo2 in repeat_part]
 
-        # Step 3: Iteratively remove productions containing nullable symbols
-        while True:
-            exists_new_null = False
-            for non_terminal, productions in self.rules.items():
-                for production in productions:
-                    null_test = production
-                    for nullable in nullable_symbols:
-                        null_test = null_test.replace(nullable, '')
-                    if null_test == '' and non_terminal not in nullable_symbols:
-                        nullable_symbols.add(non_terminal)
-                        exists_new_null = True
-            if not exists_new_null:
-                break
+        combinations = [combo1 + combo2 for combo1 in combinations for combo2 in part_combinations]
+        processed_parts.append(processed_part)
 
-        # Step 4: Replace productions containing nullable symbols
-        for non_terminal, productions in self.rules.items():
-            new_productions = []
-            for production in productions:
-                new_production = ''
-                for symbol in production:
-                    if symbol in self.rules or symbol == '':
-                        if new_production:
-                            new_productions.append(new_production)
-                            new_production = ''
-                        new_productions.append(symbol)
-                    else:
-                        new_production += symbol
-                if new_production:
-                    new_productions.append(new_production)
-            self.rules[non_terminal] = new_productions
+    return combinations, processed_parts
 
-        # Step 5: Modify productions to avoid infinite loops
-        if 'C' in self.rules:
-            self.rules['C'] = ['aC', 'bC']
+def generate_valid_combinations(regexes):
+    for regex in regexes:
+        combinations, processed_parts = generate_combinations(regex)
+        if len(combinations) > 5:
+            combinations = combinations[:5]
+        print(f"For regex {regex}: {', '.join(combinations)}")
+        print(f"Sequence of processing: {' → '.join(processed_parts)}\n")
 
-    def convertToCNF(self):
-        non_terminals = list(self.rules.keys())
+regexes = [
+    "M? N^2 (O|P)^3 Q* pow(R,+)",
+    "(x|y|z)^3  pow(8,+) (9|0)^2",
+    "(H|i) (J|k) pow(L,*)  N"
+]
 
-        for non_terminal in non_terminals:
-            productions = self.rules[non_terminal]
-            new_productions = []
-            for production in productions:
-                while len(production) > 2:
-                    first_two = production[:2]
-                    remaining = production[2:]
-                    if first_two in self.rules:
-                        new_productions.append(first_two)
-                    else:
-                        new_non_terminal = self.generateNewNonTerminal()
-                        self.rules[new_non_terminal] = [first_two]
-                        new_productions.append(new_non_terminal)
-                    production = remaining
-                new_productions.append(production)
-            self.rules[non_terminal] = new_productions
-
-    def generateNewNonTerminal(self):
-        new_non_terminal = 'X'
-        while new_non_terminal in self.rules:
-            new_non_terminal += "'"
-        return new_non_terminal
-
-    def printRules(self):
-        for f, t in self.rules.items():
-            print(f + ' -> ' + ' | '.join(t))
-
-sample_grammar = {
-    'S': ['AC', 'bA', 'B', 'aA'],
-    'A': ['', 'aS', 'ABab'],
-    'B': ['a', 'bS'],
-    'C': ['abC'],
-    'D': ['AB']
-}
-
-grammar = Grammar(sample_grammar)
-grammar.removeLambda()
-grammar.convertToCNF()
-
-grammar.printRules()
-
+generate_valid_combinations(regexes)
